1914cbfcf4ad6eb22f179625384298db8b649857
romann-broque <kinoide56@gmail.com>
2023-08-04 12:14:23 +0000

    normed

--- philo/srcs/dinner/table/philosopher/states/eat_state.c
+++ philo/srcs/dinner/table/philosopher/states/eat_state.c
@@ -6,7 +6,7 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 23:40:48 by rbroque           #+#    #+#             */
-/*   Updated: 2023/08/03 13:50:15 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/08/04 12:14:05 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,27 +35,17 @@ static void	drop_forks(t_philosopher *philo)
 		pthread_mutex_unlock(philo->forks[1]);
 }
 
-static void	reset_eat_status(t_philosopher *philo)
-{
-	set_time_since_last_dinner(philo, get_time());
-}
-
-static void	increase_meal_count(t_philosopher *philo)
-{
-	increase_nb_dinner_eaten(philo);
-}
-
 void	eat_state(t_philosopher *philo, t_dinner_config *config)
 {
 	if (grab_forks(philo) == FAILURE)
 		set_philo_state(philo, E_PREPARE_TO_DIE);
 	else if (get_philo_state(philo) != E_PREPARE_TO_DIE)
 	{
-		reset_eat_status(philo);
+		set_time_since_last_dinner(philo, get_time());
 		set_philo_state(philo, E_EAT);
 		print_philo_action(philo, EAT_MESSAGE);
 		exec_action(config->eat_time);
-		increase_meal_count(philo);
+		increase_nb_dinner_eaten(philo);
 		if (get_nb_dinner_eaten(philo) == config->nb_dinner)
 			set_philo_state(philo, E_FINISHED);
 	}
