5b5184fcf29f8b33e8a871a359c7c761a4978d28
romann-broque <kinoide56@gmail.com>
2023-09-08 08:59:24 +0000

    think wait time in progress

--- philo/srcs/dinner/table/philosopher/states/think_state.c
+++ philo/srcs/dinner/table/philosopher/states/think_state.c
@@ -6,7 +6,7 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 23:41:51 by rbroque           #+#    #+#             */
-/*   Updated: 2023/09/08 08:27:15 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/09/08 08:57:40 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,17 @@ void	think_state(
 	__attribute__((unused))t_dinner_config *config
 	)
 {
+	static size_t	think_turn = 0;
+
 	set_philo_state(philo, E_THINK);
-	exec_action(2 * config->eat_time / config->die_time);
+	if (config->nb_philosopher % 2 == 1)
+	{
+		if (think_turn == 0)
+			think_turn = philo->id;
+		if ((think_turn + philo->id) % 2 == 0)
+			exec_action(config->eat_time / 2);
+		++think_turn;
+	}
 	if (get_philo_state(philo) != E_PREPARE_TO_DIE)
 		print_philo_action(philo, THINK_MESSAGE);
 }
