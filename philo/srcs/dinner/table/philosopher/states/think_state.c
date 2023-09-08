756cf3e5bfd7e82caf9b410cd8a18a62daa1f951
romann-broque <kinoide56@gmail.com>
2023-09-08 09:27:30 +0000

    time research in progress

--- philo/srcs/dinner/table/philosopher/states/think_state.c
+++ philo/srcs/dinner/table/philosopher/states/think_state.c
@@ -6,7 +6,7 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 23:41:51 by rbroque           #+#    #+#             */
-/*   Updated: 2023/09/08 08:57:40 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/09/08 09:26:27 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,11 @@ void	think_state(
 	__attribute__((unused))t_dinner_config *config
 	)
 {
-	static size_t	think_turn = 0;
-
 	set_philo_state(philo, E_THINK);
 	if (config->nb_philosopher % 2 == 1)
 	{
-		if (think_turn == 0)
-			think_turn = philo->id;
-		if ((think_turn + philo->id) % 2 == 0)
+		if (philo->id % (config->nb_philosopher / 2))
 			exec_action(config->eat_time / 2);
-		++think_turn;
 	}
 	if (get_philo_state(philo) != E_PREPARE_TO_DIE)
 		print_philo_action(philo, THINK_MESSAGE);
