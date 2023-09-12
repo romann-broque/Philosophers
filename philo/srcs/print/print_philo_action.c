273973145546548ce9b8c532a9dd7106d806fa10
romann-broque <kinoide56@gmail.com>
2023-09-12 13:57:39 +0200

    fix death message

--- philo/srcs/print/print_philo_action.c
+++ philo/srcs/print/print_philo_action.c
@@ -6,7 +6,7 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 11:24:22 by rbroque           #+#    #+#             */
-/*   Updated: 2023/09/10 06:51:00 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/09/12 13:57:18 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ void	print_philo_action(t_philosopher *philo, const char *message)
 
 	now = delta_time(get_start_dinner_time());
 	pthread_mutex_lock(&(manager->speak_locker));
-	printf("%zu %zu %s\n", now, philo->id, message);
+	if (manager->is_a_philosopher_dead == false
+		|| get_philo_state(philo) == E_PREPARE_TO_DIE)
+		printf("%zu %zu %s\n", now, philo->id, message);
 	pthread_mutex_unlock(&(manager->speak_locker));
 }
