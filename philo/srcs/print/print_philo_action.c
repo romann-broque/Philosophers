0608e8865e850acd60b856e37b27c15306cc9d48
romann-broque <kinoide56@gmail.com>
2023-09-12 14:35:13 +0200

    fix death message for 1 philo

--- philo/srcs/print/print_philo_action.c
+++ philo/srcs/print/print_philo_action.c
@@ -6,21 +6,33 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 11:24:22 by rbroque           #+#    #+#             */
-/*   Updated: 2023/09/12 13:57:18 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/09/12 14:34:49 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	print_philo_action(t_philosopher *philo, const char *message)
+static bool	is_able_to_print(
+	t_philosopher *philosopher,
+	const t_manager *manager
+	)
+{
+	return (manager->is_a_philosopher_dead == false
+		|| get_philo_state(philosopher) == E_PREPARE_TO_DIE
+		|| get_philo_state(philosopher) == E_DEAD);
+}
+
+void	print_philo_action(
+	t_philosopher *philo,
+	const char *message
+	)
 {
 	t_manager *const	manager = get_manager();
 	size_t				now;
 
 	now = delta_time(get_start_dinner_time());
 	pthread_mutex_lock(&(manager->speak_locker));
-	if (manager->is_a_philosopher_dead == false
-		|| get_philo_state(philo) == E_PREPARE_TO_DIE)
+	if (is_able_to_print(philo, manager))
 		printf("%zu %zu %s\n", now, philo->id, message);
 	pthread_mutex_unlock(&(manager->speak_locker));
 }
